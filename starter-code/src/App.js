738a858423278838ae2255713b768ca6f6da87cf
Manami Ito <manami.j27@gmail.com>
2019-07-17 09:13:39 +0200

    done

---
+++ starter-code/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+// import FormField from "./FormField";
+// import CoolButton from "./CoolButton";
+// import CoolButton from "./Signup";
+
+import Signup from "./Signup";
+
+import Container from "./Container";
+
+
+class App extends React.Component {
+    render() {
+return(
+
+<div>
+  <Signup/>
+  <Container/>
+</div>
+);
+        
+        }
+        }
+
+export default App;
